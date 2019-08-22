85658903c083f3c437d4db4a9240435e2929a776
RevanthTolety <crevanthtolety@gmail.com>
2019-08-22 18:39:06 +0530

    msm8953-common: Add Prebuilt Gatekeeper & Keymaster Hals
    - Taken From Redmi 6 Pro [ Ysl, Daisy & Sakura Use Prebuilt hals ]

--- msm8953-common/Android.mk
+++ msm8953-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter sakura,$(TARGET_DEVICE)),)
+ifneq ($(filter daisy,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.display.color@1.0
